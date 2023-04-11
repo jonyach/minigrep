67b3ddea318e38cfede48ec413bd2dc854350211
Joseph Onyach <josephokothonyach@gmail.com>
2023-04-11 14:55:53 +0300

    Command line interface in rust

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let destination = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", destination);
+
+    let contents = fs::read_to_string(destination)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{}", contents);
+}
