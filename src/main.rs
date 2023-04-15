75e7b653842146c5569afe09cb1389fdb463bc1e
Joseph Onyach <josephokothonyach@gmail.com>
2023-04-16 02:53:58 +0300

    Building a commandline program

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
+
+    
+    // let (query, destination) = parse_config(&args);
 
     let query = &args[1];
     let destination = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", destination);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.destination);
 
     let contents = fs::read_to_string(destination)
         .expect("Should have been able to read the file");
@@ -16,3 +20,14 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
+struct Config {
+    query: String,
+    destination: String,
+}
+
+fn parse_config(args: &[String]) -> (Config) {
+    let query = args[1].clone();
+    let destination = args[2].clone();
+
+    Config {query (), destination ()}
+}
